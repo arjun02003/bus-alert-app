c31e152b9b33a45a20312338f329a4b673bfecaf
arjun02003 <arjunchowhan94@gmail.com>
2026-04-22 10:30:59 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'bus-alert-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;800&family=Outfit:wght@400;700&display=swap',
+  'https://img.icons8.com/fluency/96/bus.png'
+];
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
